507f0eaa1c8745195126e6fdcdf173587d58ccad
mathieu.devos <mathieu.devos@tut.fi>
2016-06-14 13:26:30 +0300

    Initialize welcome screens

---
+++ PinkiPonki/app/src/main/java/com/mattikettu/pinkiponki/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.mattikettu.pinkiponki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    // https://github.com/AndroidSources/Android-Splash-Screen-and-Welcome-Screen-Tutorial/
+}
